7da8b6841d05afc38b3979bf9b90b5ab01db2eb6
thesct22 <sharathct22@gmail.com>
2021-05-19 00:46:49 +0530

    Updated for multiple instances

--- terraform/basics/createinstance.tf
+++ terraform/basics/createinstance.tf
@@ -7,6 +7,11 @@
 
 
 resource "aws_instance" "MyFirstInstance" {
+    count = 2
     ami = "ami-0d563aeddd4be7fff"
     instance_type = "t2.micro"
+
+    tags = {
+      "Name" = "demo-${count.index}"
+    }
 }
